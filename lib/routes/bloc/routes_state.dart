55348896e14b57e54f83fddc4ceacbfd96fa4f46
ben <Lim6112j@gmail.com>
2025-03-07 13:33:10 +0900

    this caused update failure

--- lib/routes/bloc/routes_state.dart
+++ lib/routes/bloc/routes_state.dart
@@ -3,8 +3,8 @@ part of 'routes_bloc.dart';
 final class RouteState extends Equatable {
   const RouteState({this.latlng = const LatLng(37.2, 127.5)});
   final LatLng latlng;
-  RouteState copyWith({LatLng? latlng}) {
-    return RouteState(latlng: this.latlng);
+  RouteState copyWith({LatLng latlng = const LatLng(37.2, 127.5)}) {
+    return RouteState(latlng: latlng);
   }
 
   @override
