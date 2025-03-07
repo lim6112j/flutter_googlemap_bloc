347b1817970909713dae68d40d6662d7f88c65ce
ben <Lim6112j@gmail.com>
2025-03-07 14:20:16 +0900

    will remove stream for bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,23 @@
 import 'dart:async';
+import 'package:cielai_googlemap/routes/bloc/routes_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp());
+  runApp(const App());
 }
+class App extends StatelessWidget {
+  const App({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(create: (context) => RoutesBloc() , child: MyApp(),);
+  }
+
+}
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
